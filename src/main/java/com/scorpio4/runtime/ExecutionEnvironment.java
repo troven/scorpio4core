0a96f9087d03071d54c7d548002dfac54dfd85bf
Lee Curtis <lee.curtis@me.com>
2014-07-08 23:25:36 +1000

    repackaging runtime, core & utilities

---
+++ src/main/java/com/scorpio4/runtime/ExecutionEnvironment.java
@@ -0,0 +1,31 @@
+package com.scorpio4.runtime;
+
+import com.scorpio4.assets.AssetRegister;
+import com.scorpio4.fact.FactSpace;
+import com.scorpio4.util.Identifiable;
+import org.openrdf.repository.sail.config.RepositoryResolver;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Map;
+
+/**
+ * scorpio4-oss (c) 2014
+ * Module: com.scorpio4.runtime
+ * User  : lee
+ * Date  : 5/07/2014
+ * Time  : 9:58 PM
+ */
+public interface ExecutionEnvironment extends Identifiable {
+
+	public FactSpace getFactSpace();
+
+	public ApplicationContext getRegistry();
+
+	public AssetRegister getAssetRegister();
+
+	public RepositoryResolver getRepositoryManager();
+
+	public Map getConfig();
+
+	public void reboot() throws Exception;
+}
